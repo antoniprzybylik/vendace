56578f0f5ec1d146be21e28d783ce4d7c329b3fd
Antoni Przybylik <antoni.przybylik@wp.pl>
2023-08-18 13:23:29 +0200

    Implemented timeout.

--- src/executor.rs
+++ src/executor.rs
@@ -1,6 +1,11 @@
+use std::str::FromStr;
+use std::sync::atomic::AtomicU8;
+use std::sync::atomic::Ordering;
 use std::sync::mpsc;
+use std::time::Duration;
 
 use super::board::Board;
+use super::board::Color;
 use super::board::FENString;
 use super::book::Book;
 use super::book::BookEntry;
@@ -13,6 +18,8 @@ lazy_static! {
     static ref BOOK: Book = Book::load("/usr/share/gnuchess/smallbook.bin").unwrap();
 }
 
+static mut JOB_COUNTER: AtomicU8 = AtomicU8::new(0);
+
 pub fn executor(rx: mpsc::Receiver<String>) {
     let mut board: Board = Board::new();
 
@@ -22,7 +29,7 @@ pub fn executor(rx: mpsc::Receiver<String>) {
         match tokens[0] {
             "debug_quality" => {
                 println!("Quality = {}", board.eval());
-            },
+            }
             "position" => {
                 let mut pos_set: bool = false;
                 let mut mov_set: bool = false;
@@ -92,10 +99,68 @@ pub fn executor(rx: mpsc::Receiver<String>) {
                 }
             }
             "go" => {
-                // TODO: Zwracaj uwagę na parametry.
+                unsafe {
+                    let _ = JOB_COUNTER.fetch_add(1, Ordering::SeqCst);
+                }
+
+                // Odczytaj wtime/btime
+                let mut my_time: u64 = 8000;
+                let mut i = 1;
+                while i < tokens.len() {
+                    let token = tokens[i];
+
+                    match token {
+                        "wtime" => {
+                            if board.which_turn() == Color::White {
+                                if tokens.len() == i + 1 {
+                                    // FIXME: Malformed command error.
+                                    break;
+                                }
+
+                                if let Ok(time) = FromStr::from_str(tokens[i + 1]) {
+                                    my_time = time;
+                                } else {
+                                    // FIXME: Malformed command error.
+                                    break;
+                                }
+                            }
+                        }
+                        "btime" => {
+                            if board.which_turn() == Color::Black {
+                                if tokens.len() == i + 1 {
+                                    // FIXME: Malformed command error.
+                                    break;
+                                }
+
+                                if let Ok(time) = FromStr::from_str(tokens[i + 1]) {
+                                    my_time = time;
+                                } else {
+                                    // FIXME: Malformed command error.
+                                    break;
+                                }
+                            }
+                        }
+                        _ => {}
+                    };
+
+                    i += 1;
+                }
+
+                std::thread::spawn(move || {
+                    let job_num = unsafe { JOB_COUNTER.load(Ordering::SeqCst) };
+                    std::thread::sleep(Duration::from_millis(my_time));
+
+                    if job_num == unsafe { JOB_COUNTER.load(Ordering::SeqCst) } {
+                        unsafe {
+                            *STOP_ALL_THREADS.get_mut() = true;
+                        }
+                    }
+                });
 
                 // Odblokuj obliczenia.
-                unsafe { *STOP_ALL_THREADS.get_mut() = false; }
+                unsafe {
+                    *STOP_ALL_THREADS.get_mut() = false;
+                }
 
                 if let Some(moves) = BOOK.get(&board.hash()) {
                     let (mut best_move, mut best_weight) = (0u16, 0u16);
